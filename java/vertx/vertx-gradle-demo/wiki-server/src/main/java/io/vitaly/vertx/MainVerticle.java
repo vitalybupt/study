eb0eb2612c5118c2c64f4d91a746463d748e3e99
Zhang JunWei <JunWei.Zhang@technicolor.com>
2019-03-21 21:33:25 +0800

    update vertx example

--- java/vertx/vertx-gradle-demo/wiki-server/src/main/java/io/vitaly/vertx/MainVerticle.java
+++ java/vertx/vertx-gradle-demo/wiki-server/src/main/java/io/vitaly/vertx/MainVerticle.java
@@ -1,7 +1,16 @@
+package io.vitaly.vertx;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import io.vertx.core.json.JsonObject;
+import io.vertx.core.logging.LoggerFactory;
+import io.vertx.ext.jdbc.JDBCClient;
+import io.vertx.core.logging.Logger;
+
 public class MainVerticle extends AbstractVerticle {
     private Future<Void> prepareDatabase() {
 	Future<Void> future = Future.future();
-    
+	
 	dbClient = JDBCClient.createShared(vertx, new JsonObject()
 					   .put("url", "jdbc:hsqldb:file:db/wiki")
 					   .put("driver_class", "org.hsqldb.jdbcDriver")
@@ -10,9 +19,10 @@ public class MainVerticle extends AbstractVerticle {
 	dbClient.getConnection(ar -> {
 		if (ar.failed()) {
 		    LOGGER.error("Could not open a database connection", ar.cause());
-		    future.fail(ar.cause));
-	    })
-	    return future;
+		    future.fail(ar.cause());
+		}
+		});
+	return future;
     }
 
     private Future<Void> startHttpServer() {
@@ -21,9 +31,10 @@ public class MainVerticle extends AbstractVerticle {
     }
 
     private JDBCClient dbClient;
-    private static final logger LOGGER = LoggerFactory.getLogger(MainVerticle.class);
-    @override
-    public void start(Future<void> startFuture) throws Exception {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MainVerticle.class);
+
+    @Override
+    public void start(Future<Void> startFuture) throws Exception {
 	Future<Void> steps = prepareDatabase().compose(v -> startHttpServer());
 	steps.setHandler(startFuture.completer());
     }
