8742bd57f0abb68cf84232dbcc9b1b165adeb75e
Zhang JunWei <JunWei.Zhang@technicolor.com>
2019-04-01 19:29:42 +0800

    merge step-9

--- java/vertx/vertx-gradle-demo/wiki-server/src/main/java/io/vitaly/vertx/MainVerticle.java
+++ java/vertx/vertx-gradle-demo/wiki-server/src/main/java/io/vitaly/vertx/MainVerticle.java
@@ -1,41 +1,40 @@
-package io.vitaly.vertx;
+/*
+ *  Copyright (c) 2017 Red Hat, Inc. and/or its affiliates.
+ *  Copyright (c) 2017 INSA Lyon, CITI Laboratory.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-import io.vertx.core.AbstractVerticle;
+package io.vertx.guides.wiki;
+
+import io.reactivex.Single;
+import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Future;
-import io.vertx.core.json.JsonObject;
-import io.vertx.core.logging.LoggerFactory;
-import io.vertx.ext.jdbc.JDBCClient;
-import io.vertx.core.logging.Logger;
+import io.vertx.reactivex.core.AbstractVerticle;
 
+/**
+ * @author <a href="https://julien.ponge.org/">Julien Ponge</a>
+ */
 public class MainVerticle extends AbstractVerticle {
-    private Future<Void> prepareDatabase() {
-	Future<Void> future = Future.future();
-	
-	dbClient = JDBCClient.createShared(vertx, new JsonObject()
-					   .put("url", "jdbc:hsqldb:file:db/wiki")
-					   .put("driver_class", "org.hsqldb.jdbcDriver")
-					   .put("max_pool_size", 30));
 
-	dbClient.getConnection(ar -> {
-		if (ar.failed()) {
-		    LOGGER.error("Could not open a database connection", ar.cause());
-		    future.fail(ar.cause());
-		}
-		});
-	return future;
-    }
+  @Override
+  public void start(Future<Void> startFuture) throws Exception {
 
-    private Future<Void> startHttpServer() {
-	Future<Void> future = Future.future();
-	return future;
-    }
+    Single<String> dbVerticleDeployment = vertx.rxDeployVerticle("io.vertx.guides.wiki.database.WikiDatabaseVerticle");
 
-    private JDBCClient dbClient;
-    private static final Logger LOGGER = LoggerFactory.getLogger(MainVerticle.class);
-
-    @Override
-    public void start(Future<Void> startFuture) throws Exception {
-	Future<Void> steps = prepareDatabase().compose(v -> startHttpServer());
-	steps.setHandler(startFuture.completer());
-    }
+    DeploymentOptions opts = new DeploymentOptions().setInstances(2);
+    dbVerticleDeployment
+      .flatMap(id -> vertx.rxDeployVerticle("io.vertx.guides.wiki.http.HttpServerVerticle", opts))
+      .subscribe(id -> startFuture.complete(), startFuture::fail);
+  }
 }
