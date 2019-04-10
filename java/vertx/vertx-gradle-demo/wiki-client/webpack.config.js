30698eb00d9427087873011602caf096d705d298
Zhang JunWei <JunWei.Zhang@technicolor.com>
2019-04-10 20:44:10 +0800

    update

--- java/vertx/vertx-gradle-demo/wiki-client/webpack.config.js
+++ java/vertx/vertx-gradle-demo/wiki-client/webpack.config.js
@@ -1,8 +1,10 @@
+var path = require('path');
+
 module.exports = {
     entry: './app/wiki.js', 
     output: {
-        filename: '[name].bundle.js', 
-        path: __dirname, 
-        publicPath: __dirname
+        filename: 'wiki.bundle.js', 
+        path: path.join(__dirname, 'public/'), 
+        publicPath: __dirname,
     }
 };
