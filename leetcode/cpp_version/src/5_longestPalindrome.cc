73bb35328e8652f875b0e634df1c99382eee6681
vitaly <vitaly1979@gmail.com>
2019-09-23 01:07:36 +0800

    implement manacher's algorithm

--- leetcode/cpp_version/src/5_longestPalindrome.cc
+++ leetcode/cpp_version/src/5_longestPalindrome.cc
@@ -10,10 +10,6 @@ public:
     string longestPalindrome(string s) {
       int maxLength = 0;
       int startPos = 0;
-      vector<int> dict1;
-      vector<int> dict2;
-      int curCenter = 0;
-      int curDiameter = 0;
 
       /*
 	cur diameter is odd:          cur diameter is even:
@@ -27,41 +23,50 @@ public:
 	r = c + (diameter/2); rad = min(mirror-l, r-i);
       */
       for(int index = 0; index < s.length(); index ++) {
-	int pos1 = 0; int pos2 = 0; int mirror = 0;
-	if(curDiameter%2 == 0) {
-	  mirror = 2*curCenter - index + 1; 
-	} else {
-	  mirror = 2*curCenter - index; 
-	}
-	int l = mirror - dict1[mirror];
-	int r = curCenter + (curDiameter/2);
-	int rad = (mirror - l < r - index ? mirror - l : r - index);
-	int length1 = expand(s, index - rad , index + rad, &pos1);
-	l =  mirror - dict2[mirror];
-	rad = (mirror - l < r - index ? mirror - l : r - index);
-	int length2 = expand(s, index - rad + 1, index + rad, &pos2);
+	int pos1 = 0; int pos2 = 0; int rad = 0;
+	rad = calRad(index, dict1);
+	int length1 = expand(s, index - rad - 1, index + rad + 1, &pos1);
+
+	rad = calRad(index, dict2);
+	int length2 = expand(s, index - rad , index + rad + 1, &pos2);
 	dict1.push_back(length1/2);dict2.push_back(length2/2);
 	if(length1 > length2) {
+	  if(index + length1/2 > curCenter + curDiameter/2) {
+	    curCenter = index;
+	    curDiameter = length1;
+	  }
 	  if(length1 > maxLength) {
 	    maxLength = length1;
 	    startPos = pos1;
 	  }
 	} else {
+	  if(index + length2/2 > curCenter + curDiameter/2) {
+	    curCenter = index;
+	    curDiameter = length2;
+	  }
 	  if(length2> maxLength) {
 	    maxLength = length2;
 	    startPos = pos2;
 	  }
 	}
       }
-      for(const int & rad : dict1) {
-	std::cout << rad << std::endl;
-      }
-      for(const int & rad : dict2) {
-	std::cout << rad << std::endl;
-      }
       return s.substr(startPos, maxLength);
     }
 private:
+  int calRad(int index, vector<int> &dict){
+    if(index <= curCenter || index >= curCenter + (curDiameter/2))
+      return 0;
+    int mirror = 0;
+    if(curDiameter%2 == 0) {
+      mirror = 2*curCenter - index + 1; 
+    } else {
+      mirror = 2*curCenter - index; 
+    }
+    int l = mirror - dict[mirror];
+    int r = curCenter + (curDiameter/2);
+    return (mirror - l < r - index ? mirror - l : r - index);
+  }
+  
   int expand(string s, int backward, int forward, int* pos) {
     while(backward >= 0 && forward < s.length() && s[backward] == s[forward]) {
       backward--;
@@ -70,11 +75,17 @@ private:
     *pos = backward+1;
     return (forward - backward - 1);
   }
+
+private:
+  vector<int> dict1;
+  vector<int> dict2;
+  int curCenter = 0;
+  int curDiameter = 0;
 };
 
 int main () {
   class Solution sol;
-  string ret = sol.longestPalindrome("cbba");
-  std::cout << ret << std::endl;
+  string ret = sol.longestPalindrome("321012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210012321001232100123210123210012321001232100123210123");
+  //std::cout << ret << std::endl;
   return 0;
 }
